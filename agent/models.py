a69320b94c7be791d7a823892f7755ea814c6404
Dmitry Roitman <dmitryro@gmail.com>
2017-05-11 17:53:45 -0400

    flake8 format

--- agent/models.py
+++ agent/models.py
@@ -3,13 +3,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Agent Type model
+
+
 class AgentType(models.Model):
+
     agent_type = models.CharField(max_length=100, blank=True, null=True)
     agent_code = models.CharField(max_length=100, blank=True, null=True)
 
 # Auction model
+
+
 class Agent(models.Model):
-    reputation = models.FloatField(default=0,blank=True,null=True) 
-    agent_type = models.ForeignKey(AgentType,blank=True,null=True)
+
+    reputation = models.FloatField(default=0, blank=True, null=True)
+    agent_type = models.ForeignKey(AgentType, blank=True, null=True)
     time_activated = models.DateTimeField(auto_now_add=True)
-    time_passivated = models.DateTimeField(blank=True,null=True)
+    time_passivated = models.DateTimeField(blank=True, null=True)
+    time_terminated = models.DateTimeField(blank=True, null=True)
